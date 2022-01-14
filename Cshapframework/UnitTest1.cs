02c7074078fc4efc5d1f5657564ce1b2cd09b1dc
Sachin <Sachin@testingxpert.com>
2022-01-14 22:47:17 +0530

    Add project files.

---
+++ Cshapframework/UnitTest1.cs
@@ -0,0 +1,23 @@
+using NUnit.Framework;
+
+namespace Cshapframework
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+        [TearDown]
+        public void quit()
+        {
+
+        } 
+    }
+}
